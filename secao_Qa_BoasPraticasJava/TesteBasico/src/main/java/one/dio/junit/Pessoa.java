eb1250c94f65ad1284ea39d1362b0824957afcf0
og3da <rapha488@gmail.com>
2024-01-18 09:39:35 -0300

    testes classe Pessoa

--- secao_Qa_BoasPraticasJava/TesteBasico/src/main/java/one/dio/junit/Pessoa.java
+++ secao_Qa_BoasPraticasJava/TesteBasico/src/main/java/one/dio/junit/Pessoa.java
@@ -1,2 +1,23 @@
-package one.dio.junit;public class Pessoa {
+package one.dio.junit;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Pessoa {
+
+    private String nome;
+    private LocalDate nascimento;
+
+    public Pessoa(String nome, LocalDate nascimento) {
+        this.nome = nome;
+        this.nascimento = nascimento;
+    }
+
+    public int getIdade() {
+        return (int) ChronoUnit.YEARS.between(nascimento, LocalDate.now());
+    }
+
+    public boolean ehMaiorDeIdade() {
+        return getIdade() >= 18;
+    }
 }
